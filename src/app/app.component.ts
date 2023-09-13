254dbff4e1ed130d7847a948f6a4e37f50a87c95
Rongeard Benoit <benoit.rongeard@gmail.com>
2023-09-13 23:00:52 +0200

    Adding table to manage Users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface User {
+  id: number,
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+}
+
+const USERS: User[] = [
+  { id: 1, firstName: "Alice", lastName: "Smith", age: 25, email: "alice.smith@example.com" },
+  { id: 2, firstName: "Bob", lastName: "Johnson", age: 30, email: "bob.johnson@example.com" },
+  { id: 3, firstName: "Charlie", lastName: "Brown", age: 40, email: "charlie.brown@example.com" },
+  { id: 4, firstName: "David", lastName: "Lee", age: 20, email: "david.lee@example.com" },
+  { id: 5, firstName: "Emily", lastName: "Wang", age: 27, email: "emily.wang@example.com", }
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-decorator-pop-up';
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'age', 'delete'];
+  dataSource = USERS;
+
+  deleteUser(element: User) {
+    this.dataSource = this.dataSource.filter(user => user.id != element.id);
+  }
+
+  reloadData() {
+    this.dataSource = USERS;
+  }
 }
